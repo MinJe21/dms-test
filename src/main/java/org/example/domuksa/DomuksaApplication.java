e3d07c61c289a4fa99c2907b2609576787e76b45
김민제 <kimminje@gimminje-ui-MacBookPro.local>
2026-01-28 19:13:59 +0900

    create

---
+++ src/main/java/org/example/domuksa/DomuksaApplication.java
@@ -0,0 +1,13 @@
+package org.example.domuksa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomuksaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomuksaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/domuksa/DomuksaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.domuksa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomuksaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
